fab1a9ea3acbd9c50d43e3376a0618483b6cf127
pornoiya <49945883+pornoiya@users.noreply.github.com>
2021-03-22 15:34:17 +0500

    Update students_base.py

--- students_base.py
+++ students_base.py
@@ -16,8 +16,8 @@ class StudentsBase(DataBase):
         self.cur.execute(f"CREATE SEQUENCE IF NOT EXISTS counter START 1;")
         self.cur.execute(f"CREATE TABLE IF NOT EXISTS {self.table_name}"
                          f" (id INTEGER, full_name TEXT, rating FLOAT, "
-                         f"age INTEGER, photo BYTEA, specialty TEXT, "
-                         f"student_group TEXT, sex CHAR(1), colour CHAR(7))")
+                         f"age INTEGER, photo TEXT, specialty TEXT, "
+                         f"student_group TEXT, sex CHAR(1), colour CHAR(7), email TEXT)")
         self.conn.commit()
 
     def add_student(self, student: Student):
