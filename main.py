810108d665ac370530f7e440279b52fc880ed5c7
pornoiya <49945883+pornoiya@users.noreply.github.com>
2021-03-26 18:17:11 +0500

    Update main.py
    added reasons why in main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from web.view import app, db
 from config import config
 
+# Почему фласк? В университете все веб приложения я писала на фласке. Я уже знакома с этим
+# фреймворком, поэтому выбрала его.
+# Почему postgres? Я умею работать с постгресом через psql, это полезно когда база на удаленном
+# сервере.
+
 if __name__ == "__main__":
     app.run(debug=config.APP_DEBUG,
             host=config.APP_HOST, port=config.APP_PORT)
